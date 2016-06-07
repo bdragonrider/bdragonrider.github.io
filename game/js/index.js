71354a86c1d66ffa24d87994deb59f7e15aa5fed
bdragonrider <bvoss16@verizon.net>
2016-06-07 17:55:08 -0400

    Added Game folder

---
+++ game/js/index.js
@@ -0,0 +1,31 @@
+// var textBox = document.getElementById("text");
+
+//  function gotwo() {
+//    alert("heyyy");
+//    textBox.innerHTML = "heyyyyyyyyy yos";
+// }
+ 
+function Death1() {
+
+  alert("You Are shot in the heel with a poisonous arrow, Oh wait you're not Achilles then...  You get Hit with a bullet. Oops... this isn't modern How About a... No... You were shot through the heart and you're to blame 'cause you give love a bad name then you bled to death D:");
+}
+
+// function ans(){ 
+//   memory = alert()
+//     "Welcome to Dragonland, I suppose you are here because King Bdragonrider summoned, you his palace is that way.");
+//   if (memory);}
+    
+
+  function go(){
+    alert("Welcome to DragonLand.");
+     if (memory){ 
+       ans();
+     }else  {
+      memory = confirm("Would you like to play");
+     }
+    if (memory){
+      
+    } else {
+      Death1();
+     }
+   }
